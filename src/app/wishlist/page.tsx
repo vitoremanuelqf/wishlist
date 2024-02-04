821835dcc2ea8597cfecf5c65572c9b0a0ef133d
Vítor Emanuel Queiroz Ferreira <vitoremanuelqf@gmail.com>
2024-02-04 17:15:29 -0300

    :sparkles: feat - Adicionado o loading para o carregamento dos itens na wishlist.

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -7,7 +7,7 @@ import { Product } from '~/components/Product'
 import { useWishlist } from '~/hooks/useWishlist'
 
 export default function Wishlist() {
-  const { wishlist } = useWishlist()
+  const { loading, wishlist } = useWishlist()
 
   return (
     <main className="flex h-auto w-full justify-center bg-white">
@@ -19,7 +19,19 @@ export default function Wishlist() {
           ]}
         />
 
-        {wishlist.length > 0 && (
+        {loading && (
+          <div className="animate__animated animate__zoomIn w=full flex h-auto max-w-96 flex-col items-center justify-center gap-2 py-6 sm:py-8">
+            <div className="flex h-min w-min items-center justify-center rounded-full border border-gray-700 bg-[#5b2b84] p-4">
+              <FiInbox className="animate__animated animate__heartBeat animate__infinite h-8 w-8 text-white" />
+            </div>
+
+            <h2 className="text-center text-base font-medium text-gray-700 sm:text-xl">
+              Carregando...
+            </h2>
+          </div>
+        )}
+
+        {wishlist.length > 0 && !loading && (
           <div className="grid grid-cols-2 gap-2 sm:gap-4 md:grid-cols-3 lg:grid-cols-4">
             {wishlist.map((product) => (
               <Product
@@ -31,7 +43,7 @@ export default function Wishlist() {
           </div>
         )}
 
-        {wishlist.length <= 0 && (
+        {wishlist.length <= 0 && !loading && (
           <div className="animate__animated animate__zoomIn w=full flex h-auto max-w-96 flex-col items-center justify-center gap-2 py-6 sm:py-8">
             <div className="flex h-min w-min items-center justify-center rounded-full border border-gray-700 bg-[#5b2b84] p-4">
               <FiInbox className="h-12 w-12 text-white" />
