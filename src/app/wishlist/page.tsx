9ca5bbe969d9068100565391c4a85c0fdd233616
Vítor Emanuel Queiroz Ferreira <vitoremanuelqf@gmail.com>
2024-02-04 17:27:56 -0300

    :recycle: feat - Componentização do Loading e criação do teste.

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -3,6 +3,7 @@
 import { FiInbox } from 'react-icons/fi'
 
 import { Breadcrumb } from '~/components/Breadcrumb'
+import { Loading } from '~/components/Loading'
 import { Product } from '~/components/Product'
 import { useWishlist } from '~/hooks/useWishlist'
 
@@ -19,17 +20,7 @@ export default function Wishlist() {
           ]}
         />
 
-        {loading && (
-          <div className="animate__animated animate__zoomIn w=full flex h-auto max-w-96 flex-col items-center justify-center gap-2 py-6 sm:py-8">
-            <div className="flex h-min w-min items-center justify-center rounded-full border border-gray-700 bg-[#5b2b84] p-4">
-              <FiInbox className="animate__animated animate__heartBeat animate__infinite h-8 w-8 text-white" />
-            </div>
-
-            <h2 className="text-center text-base font-medium text-gray-700 sm:text-xl">
-              Carregando...
-            </h2>
-          </div>
-        )}
+        {loading && <Loading />}
 
         {wishlist.length > 0 && !loading && (
           <div className="grid grid-cols-2 gap-2 sm:gap-4 md:grid-cols-3 lg:grid-cols-4">
